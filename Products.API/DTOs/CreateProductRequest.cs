6a44853d44cf2fdbed9a194ae5cb5d52f17519d3
Mora Fuchs <morafacudrive@gmail.com>
2026-05-08 19:56:28 -0300

    Listo para pruebas

--- Products.API/DTOs/CreateProductRequest.cs
+++ Products.API/DTOs/CreateProductRequest.cs
@@ -1,16 +1,10 @@
-﻿using System;
+﻿namespace Products.API.DTOs;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-
-	public CreateProductRequest()
-	{
-
-         public string Nombre { get; set; } = string.Empty;
-        public string? Descripcion { get; set; }
-        public decimal Precio { get; set; }
-        public int Stock { get; set; }
-        public string Categoria { get; set; } = string.Empty;
+public class CreateProductRequest
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string Categoria { get; set; } = string.Empty;
 }
-
--- Products.API/DTOs/ProductResponse.cs
+++ Products.API/DTOs/ProductResponse.cs
@@ -1,19 +1,12 @@
-﻿using System;
+﻿namespace Products.API.DTOs;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
 public class ProductResponse
 {
-	public ProductResponse()
-	{
-
-        public Guid Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string? Descripcion { get; set; }
-        public decimal Precio { get; set; }
-        public int Stock { get; set; }
-        public string Categoria { get; set; } = string.Empty;
-        public DateTime FechaCreacion { get; set; }
-}
+    public Guid Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Stock { get; set; }
+    public string Categoria { get; set; } = string.Empty;
+    public DateTime FechaCreacion { get; set; }
 }
