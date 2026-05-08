9ef8012b09a5ddb19018f395b2d6203c7067035a
Mora Fuchs <morafacudrive@gmail.com>
2026-05-08 20:33:08 -0300

    Users.cs

---
+++ Users.API/Program.cs
@@ -0,0 +1,19 @@
+using Users.API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<UserService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
