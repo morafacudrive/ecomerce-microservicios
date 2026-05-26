c4f75877d353d3a7d1417fa7d592489cb4f74b05
Agustin Orlando Palermo <agustinorlando2009@hotmail.com>
2026-05-26 15:08:57 -0300

    ExceptionHandlers y Validations en Cart.API

--- Cart.API/DTOs/AddItemRequest.cs
+++ Cart.API/DTOs/AddItemRequest.cs
@@ -1,7 +1,13 @@
-﻿namespace Cart.API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cart.API.DTOs;
 
 public class AddItemRequest
 {
+    [Required(ErrorMessage = "El ProductoId es requerido.")]
     public Guid ProductoId { get; set; }
+
+    [Required(ErrorMessage = "La cantidad es requerida.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad inválida.")]
     public int Cantidad { get; set; }
 }
--- Cart.API/DTOs/UpdateItemRequest.cs
+++ Cart.API/DTOs/UpdateItemRequest.cs
@@ -1,6 +1,10 @@
-﻿namespace Cart.API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cart.API.DTOs;
 
 public class UpdateItemRequest
 {
+    [Required(ErrorMessage = "La cantidad es requerida.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad inválida.")]
     public int Cantidad { get; set; }
 }
