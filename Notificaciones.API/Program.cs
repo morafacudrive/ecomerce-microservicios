d937f6f668e986a4107fc22f821a983afba6dc1e
Agustin Orlando Palermo <agustinorlando2009@hotmail.com>
2026-05-26 11:09:27 -0300

    ExceptionHandlers y Validations en Notifications.API

--- Notificaciones.API/Program.cs
+++ Notificaciones.API/Program.cs
@@ -1,12 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
 using Notifications.API.Services;
+using Notifications.API.ExceptionHandlers;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errores = context.ModelState
+            .Where(x => x.Value?.Errors.Count > 0)
+            .SelectMany(x => x.Value!.Errors.Select(e => e.ErrorMessage))
+            .ToList();
+
+        var mensaje = string.Join("; ", errores);
+
+        return new BadRequestObjectResult(new
+        {
+            type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            title = "Bad Request",
+            status = 400,
+            detail = "La solicitud contiene datos inválidos.",
+            instance = context.HttpContext.Request.Path.Value,
+            errorCode = "NTF-002",
+            errorMessage = mensaje
+        });
+    };
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<NotificationService>();
+
+// Exception Handlers
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
+builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -14,6 +49,9 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+// Activa el manejo global de excepciones
+app.UseExceptionHandler();
+
 app.MapControllers();
 
 app.Run();
